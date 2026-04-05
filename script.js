cf5b6c9e1d046fa36168374f0aa96a14fc818bbf
Debdut <debdutjana222@gmail.com>
2026-04-05 18:36:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ const SITE_CONFIG = {
 // Easy placeholders for student names + links
 const STUDENTS = [
   {
-    name: "Debdut Jana",
+    name: "Student Name 1",
     image: "",
     department: "ECE",
     year: "2nd Year",
